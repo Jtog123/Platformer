adff713c2482f45cf0ab8012ae22545c93d2471c
jttest github <jamestogher567@gmail.com>
2023-12-17 21:57:03 -0800

    loop

--- game.py
+++ game.py
@@ -1,1 +1,23 @@
 import pygame
+import sys
+
+
+screen = pygame.display.set_mode((300,300))
+clock = pygame.time.Clock
+
+running = True
+while running:
+
+    clock.tick(60)
+    screen.fill((0,0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    
+    pygame.display.update()
+
+    
+
+    
