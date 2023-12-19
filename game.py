94fc312de16f0b8baafa169a5ed8a6d7dba9bd95
jttest github <jamestogher567@gmail.com>
2023-12-18 17:13:03 -0800

    set up structure

--- game.py
+++ game.py
@@ -1,23 +1,56 @@
 import pygame
 import sys
 
+class PhysicsEntity:
+    def __init__(self, game, e_type, pos, size) -> None:
+        self.game = game
+        self.screen = self.game.screen
+        self.e_type = e_type
+        self.position = list(pos)
+        self.size = size
+        self.velocity = [0,0]
 
-screen = pygame.display.set_mode((300,300))
-clock = pygame.time.Clock
+    def update(self, movement = (0,0)):
 
-running = True
-while running:
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
 
-    clock.tick(60)
-    screen.fill((0,0,0))
+        self.position[0] += frame_movement[0]
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+        self.position[1] += frame_movement[1]
+    
+    def render(self,surface):
+        pass
+
+
+
+
+class Game:
+    def __init__(self) -> None:
+        
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((640,320))
+        self.clock = pygame.time.Clock()
+
+
+        self.assets = {}
 
     
-    pygame.display.update()
+    def update(self):
+        pass
 
+    def run(self):
+        while True:
+            self.clock.tick(60)
+            self.screen.fill((0,0,0))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+
+Game().run()
     
 
     
