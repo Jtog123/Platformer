86f0b17b9d67e236741aadf493e93358533f1367
jttest github <jamestogher567@gmail.com>
2023-12-19 20:02:55 -0800

    not fixed

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
+
 class PhysicsEntity:
     def __init__(self, game, e_type, pos, size) -> None:
         self.game = game
@@ -18,10 +21,10 @@ class PhysicsEntity:
 
         self.position[1] += frame_movement[1]
     
-    def render(self,surface):
-        pass
+    def render(self, surface):
+        surface.blit(self.game.assets['player'], self.position)
 
-
+#Next we need to design the tile for the floor
 
 
 class Game:
@@ -31,9 +34,17 @@ class Game:
 
         self.screen = pygame.display.set_mode((640,320))
         self.clock = pygame.time.Clock()
+        self.movement = [False, False]
 
+        self.player = PhysicsEntity(self,'player', (50,50),(14,18))
+        
+        self.assets = {
+            'player': load_image('entities\player\idle\\0.png'),
+            'stone': load_images('tiles\stone\\')
+        }
+        self.tilemap = Tilemap(self, tile_size=16)
 
-        self.assets = {}
+        print(self.assets['stone'][0])
 
     
     def update(self):
@@ -41,13 +52,31 @@ class Game:
 
     def run(self):
         while True:
-            self.clock.tick(60)
-            self.screen.fill((0,0,0))
+            
+            self.screen.fill((100,10,10))
+            self.tilemap.render(self.screen)
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+
+            pygame.display.update()
+            self.clock.tick(60)
                 
 
 Game().run()
---
+++ scripts/tilemap.py
@@ -0,0 +1,64 @@
+import pygame
+import json
+
+PHYSICS_TILES = {'grass', 'stone'}
+NEIGHBOR_OFFSETS = [(-1,0), (-1,-1), (0,-1), (1,-1), (1,0), (0,0), (-1,1), (0,1), (1,1)]
+
+
+class Tilemap:
+    def __init__(self, game, tile_size = 16) -> None:
+        self.game = game
+        self.tile_size = tile_size
+        self.tilemap = {}
+        self.offgrid_tiles = []
+
+        for i in range(10):
+            self.tilemap[str(3 + i) + ';20'] = {'type': 'stone', 'variant':1, 'pos': (3 + i, 20)}
+
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0]) // self.tile_size, int(pos[1] // self.tile_size))
+
+        for offset in NEIGHBOR_OFFSETS:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles
+    
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile['type'] in PHYSICS_TILES:
+                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size , tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
+        return rects
+    
+    def render(self, surface):
+        for tile in self.offgrid_tiles:
+            surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0], tile['pos'][1]))
+
+        for location in self.tilemap:
+            tile = self.tilemap[location]
+            surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
+
+            
+        
+
+
+'''
+    
+    def render(self,surface, offset = (0,0)):
+        for tile in self.offgrid_tiles:
+            #blitting this type of tile at a specific location
+            surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
+            
+        
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            #print(tile)
+            #blit each tile by type and variant, at a specfic position
+            surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
+            #print(self.game.assets[tile['type']][tile['variant']], ([tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size]) )
+    
+        
+
+'''
